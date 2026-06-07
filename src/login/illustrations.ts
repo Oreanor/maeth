893dc54e1a92165a7a622006ecc091f987f71ebe
Sergey Nikonets <oreanor@gmail.com>
2026-06-07 22:24:07 +0100

    pic06

--- src/login/illustrations.ts
+++ src/login/illustrations.ts
@@ -5,6 +5,7 @@ export const LOGIN_ILLUSTRATIONS = [
   '/pic/03.png',
   '/pic/04.png',
   '/pic/05.png',
+  '/pic/06.png',
 ] as const
 
 export function pickLoginIllustration(): (typeof LOGIN_ILLUSTRATIONS)[number] {
